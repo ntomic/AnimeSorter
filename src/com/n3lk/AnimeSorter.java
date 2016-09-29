7753640aea6b8d444d4a02c796f90cf255efb6a1
n3lk <nella_co@yahoo.com>
2016-09-29 23:29:33 +0200

    formatter

--- src/com/n3lk/AnimeSorter.java
+++ src/com/n3lk/AnimeSorter.java
@@ -2,29 +2,52 @@ package com.n3lk;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Properties;
 import java.util.logging.*;
 import static java.nio.file.Files.move;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
  class AnimeSorter {
      private static final Logger LOGGER = Logger.getLogger( AnimeSorter.class.getName() );
+     private Properties properties = new Properties();
+     private final String propertiesFile = "config.properties";
+
+     // Load Properites from config.properties
+     private void loadProperties() throws IOException {
+         InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(propertiesFile);
+         properties.load(resourceStream);
+         resourceStream.close();
+     }
 
      // Constructor initializes: Logger - Handler - Formatter
-     AnimeSorter(String logDirectory) {
-        try {
+     AnimeSorter()
+     {
+
+         //Location of log file
+
+         try {
+             loadProperties();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+
+         String logProperty = properties.getProperty("logProperty");
+
+         try {
             /** @see <a href="http://docs.oracle.com/javase/7/docs/api/java/util/logging/FileHandler.html">FileHandler</a> for more on FileHandler function
             FileHandler(String pattern,int limit,int count, boolean append)
-            @param pattern the pattern for naming the output file
-            @param limit the maximum number of bytes to write to any one file
-            @param count the number of files to use
-            @param append specifies append mode
+            *param pattern the pattern for naming the output file
+            *param limit the maximum number of bytes to write to any one file
+            *param count the number of files to use
+            *param append specifies append mode
             **/
             // Create handler, give location for the log file
-            FileHandler fh = new FileHandler(logDirectory,1000000,10,true);
+            FileHandler fh = new FileHandler(logProperty,1000000,10,true);
             // Setting up the log Formatter. SimpleFormatter() can be used instead of overriding.
             // https://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html
             fh.setFormatter(new Formatter() {
@@ -53,30 +76,31 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
      /**
      *  Moves files that begin with "[HorribleSubs]" and end with ".mkv"
-     *  @param sourceDirectory The source directory of the files
-     *  @param targetDirectory The target directory of the files
      *  @throws IOException If an input or output exception occurred
      *
      **/
-     void moveFiles(Path sourceDirectory, Path targetDirectory) throws IOException {
+       void moveFiles() throws IOException {
 
-        //A pattern to match over each file (must be "\\" instead of "/")
+         //Getting source/target properties from config.properties
+         String sourceProperty = properties.getProperty("sourceProperty");
+         String targetProperty = properties.getProperty("targetProperty");
+
+         // Creating Path objects
+         Path sourceDirectory = Paths.get(sourceProperty.toString());
+         Path targetDirectory = Paths.get(targetProperty.toString());
+
+         //A pattern to match over each file (must be "\\" instead of "/")
         String pattern = "**\\[HorribleSubs]*.mkv";
         /**
          * Opens a directory, returning a DirectoryStream to iterate over the entries in the directory.
          * The elements returned by the directory stream's iterator are of type Path, each one representing an entry in the directory.
          * The entries returned by the iterator are filtered by matching the String representation of their file names against the given globbing pattern.
-         *  @see <a href="https://docs.oracle.com/javase/tutorial/essential/io/fileOps.html#glob">What is glob</a> for more on glob pattern matching
-         *  @param sourceDirectory Directory over which to iterate
-         *  @param pattern A glob pattern to match over each file
-         *  @return a new and open DirectoryStream object
-         *  @throws IOException  Runtime exception thrown if an I/O error is encountered when iterating over the entries in a directory.
-         *
+         * https://docs.oracle.com/javase/tutorial/essential/io/fileOps.html#glob
          * **/
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(sourceDirectory, pattern)) {
 
             for (Path entry: stream) {
-                LOGGER.info("File Transfered: " + entry.getFileName().toString());
+                // LOGGER.info("File Transfered: " + entry.getFileName().toString());
                 String input = entry.getFileName().toString();
                 File destFolder = new File( targetDirectory.toString() + '/' + entry.getFileName().toString().substring(input.indexOf(']')+2,input.lastIndexOf('-')-1));
 
@@ -84,11 +108,11 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
                 try {
                     if(destFolder.mkdir()) {
-                        LOGGER.info("New Directory Created");
+                       // LOGGER.info("New Directory Created");
                     } else {
-                        LOGGER.info("New Directory Not Created");
+                       // LOGGER.info("New Directory Not Created");
                     }
-                    LOGGER.info("File Move Destination: " + destPath.toString());
+                   // LOGGER.info("File Move Destination: " + destPath.toString());
                     move(entry,destPath, REPLACE_EXISTING);
                 } catch (IOException ex) {
                     LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
@@ -105,20 +129,17 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
     }
 
     public static void main(String[] args) {
-        //Location of source directory, target directory
-        Path sourceDirectory = Paths.get("C:/Users/n3lk/testing/source/");
-        Path targetDirectory = Paths.get("C:/Users/n3lk/testing/target/");
-        //Location of log file
-        String logDirectory = "C:/Users/n3lk/IdeaProjects/AnimeSorter/log/AnimeSorter.log";
-
-        AnimeSorter sorterObject = new AnimeSorter(logDirectory);
         try {
-            sorterObject.moveFiles(sourceDirectory,targetDirectory);
+        AnimeSorter sorterObject = new AnimeSorter();
+        sorterObject.moveFiles();
+
         } catch (Exception ex) {
             LOGGER.log( Level.SEVERE, "Exception: " + ex.toString(), ex );
             ex.printStackTrace();
         }
-        LOGGER.info("***AnimeSorter Process Finished***");
+        //LOGGER.info("***AnimeSorter Process Finished***");
     }
 
-}
+
+
+ }
