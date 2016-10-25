a16b9315a07be30fa8306d15700dfd9ee733c9d6
n3lk <nela.tomic@gmail>
2016-10-25 21:36:03 +0200

    when move fails, files get copied

--- src/com/n3lk/AnimeSorter.java
+++ src/com/n3lk/AnimeSorter.java
@@ -1,6 +1,4 @@
 package com.n3lk;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.*;
@@ -9,97 +7,153 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Properties;
 import java.util.logging.*;
-
-import static java.nio.file.Files.move;
+import static java.nio.file.Files.*;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
-class AnimeSorter {
+class AnimeSorter
+{
+	private static final Logger LOGGER = Logger.getLogger(AnimeSorter.class.getName());
+	private Properties properties = new Properties();
+	private final String propertiesFile = "config.properties";
 
-    private static final Logger LOGGER = Logger.getLogger(AnimeSorter.class.getName());
-    private Properties properties = new Properties();
-    private final String propertiesFile = "config.properties";
+	private void loadProperties() throws IOException
+	{
+		InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(propertiesFile);
+		properties.load(resourceStream);
+		resourceStream.close();
+	}
 
-    private void loadProperties() throws IOException {
-        InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(propertiesFile);
-        properties.load(resourceStream);
-        resourceStream.close();
-    }
+	private static boolean tryMove(Path startPath, Path destinationPath) {
+		try
+		{
+			move(startPath, destinationPath, REPLACE_EXISTING);
+			LOGGER.info("Moved File " + startPath.getFileName());
+			return true;
+		}
+		catch(IOException ex) {
+			LOGGER.log(Level.SEVERE, "Exception: locked file \nException message: " + ex.getMessage());
+			return false;
+		}
+	}
 
-    private AnimeSorter() throws IOException {
-        // Load Properties from config.properties file
-        loadProperties();
+	private AnimeSorter()
+	{
+		try
+		{
+			// Load Properties from config.properties file
+			loadProperties();
+			// Get properties for the log file
+			String logProperty = properties.getProperty("logProperty");
 
-        // Logging initialization
-        try {
-            // Get properties for the log file
-            String logProperty = properties.getProperty("logProperty");
-            // Create FileHandler(location_of_log_ file + log_file_name, file_size_in_bytes, no_of_files_to_keep, bool_append)
-            FileHandler fh = new FileHandler(logProperty + "AnimeSorter.log", 1000000, 10, true);
-            // Setting up the log Formatter. SimpleFormatter() can be used instead of overriding.
-            fh.setFormatter(new Formatter() {
-                @Override
-                public String format(LogRecord record) {
-                    SimpleDateFormat logTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    Calendar cal = new GregorianCalendar();
-                    cal.setTimeInMillis(record.getMillis());
-                    return logTime.format(cal.getTime()) + " "  //gets formatted DateTime
-                            + record.getLevel() + " "  //gets level of log entry: info, fine,..., severe
-                            + record.getSourceClassName().substring(record.getSourceClassName().lastIndexOf(".") + 1, record.getSourceClassName().length()) //gets formatted class name
-                            + "."
-                            + record.getSourceMethodName() //gets method name that called logger
-                            + "() : "
-                            + record.getMessage() + "\n"; //gets message
-                }
-            });
-            //add handler to logger
-            LOGGER.addHandler(fh);
-            LOGGER.info("***AnimeSorter Log Init***");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+			// Create a Directory for the Log file
+			Path logPath = Paths.get(logProperty);
+			boolean logExisted = true;
+			if(notExists(logPath))
+			{
+				Files.createDirectory(logPath);
+				logExisted = false;
 
-    private void moveFiles() throws IOException {
-        // Getting properties from config.properties file
-        String sourceDirectory = properties.getProperty("sourceProperty");
-        String targetDirectory = properties.getProperty("targetProperty");
-        String pattern = properties.getProperty("patternProperty");
+			}
 
-        // newDirectoryStream opens a directory, returning a DirectoryStream to iterate over the entries in the directory.
-        // The entries returned by the iterator are filtered by matching the String representation of their file names against the given pattern.
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(sourceDirectory), pattern)) {
-            for (Path entry : stream) {
-                String tempInput = entry.getFileName().toString();  // getFileName() returns highest file name in directory hierarchy
-                // tempInput = "[HorribleSubs] Hunter x Hunter - 138 [720p]" => shortFileName = "Hunter x Hunter"
-                String shortFileName = tempInput.substring(tempInput.indexOf(']') + 2, tempInput.lastIndexOf('-') - 1);
-                File destinationFolder = new File(targetDirectory + shortFileName);
+			// Create FileHandler(location_of_log_ file + log_file_name, file_size_in_bytes, no_of_files_to_keep, bool_append)
+			FileHandler fh = new FileHandler(logProperty + "AnimeSorter.log", 1000000, 10, true);
+			// Setting up the log Formatter. SimpleFormatter() can be used instead of overriding.
+			fh.setFormatter(new Formatter()
+			{
+				@Override
+				public String format(LogRecord record)
+				{
+					SimpleDateFormat logTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+					Calendar cal = new GregorianCalendar();
+					cal.setTimeInMillis(record.getMillis());
+					return logTime.format(cal.getTime()) + " "  //gets formatted DateTime
+							+ record.getLevel() + " "  //gets level of log entry: info, fine,..., severe
+							+ record.getSourceClassName().substring(record.getSourceClassName().lastIndexOf(".") + 1, record.getSourceClassName().length()) //gets formatted class name
+							+ "."
+							+ record.getSourceMethodName() //gets method name that called logger
+							+ "() : "
+							+ record.getMessage() + "\n"; //gets message
+				}
+			});
+			// Add handler to logger
+			LOGGER.addHandler(fh);
 
-                Path destinationPath = destinationFolder.toPath().resolve(entry.getFileName()); // resolve() combines two paths
-                try {
-                    if (destinationFolder.mkdirs()) LOGGER.info("Created Folder " + destinationFolder);
-                    move(entry, destinationPath, REPLACE_EXISTING); // Path move(Path source, Path target, CopyOption option)
-                    LOGGER.info(tempInput + " MOVED TO " + destinationFolder);
-                } catch (IOException ex) {
-                    LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
-                    ex.printStackTrace();
-                }
-            }
-        } catch (DirectoryIteratorException ex) {
-            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
-            throw ex.getCause();
-        }
-    }
+			if(!logExisted)LOGGER.info("Created log folder: " + logPath );
+			LOGGER.info("***AnimeSorter Log Init***");
 
-    public static void main(String[] args) {
-        try {
-            AnimeSorter sorterObject = new AnimeSorter();
-            sorterObject.moveFiles();
+		} catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 
-        } catch (Exception ex) {
-            LOGGER.log(Level.SEVERE, "Exception: " + ex.toString(), ex);
-            ex.printStackTrace();
-        }
-        //LOGGER.info("***AnimeSorter Process Finished***");
-    }
+	private void moveFiles() throws IOException
+	{
+		// Getting properties from config.properties file
+		String sourceDirectoryString = properties.getProperty("sourceProperty");
+		String targetDirectoryString = properties.getProperty("targetProperty");
+		String pattern = properties.getProperty("patternProperty");
+
+		Path targetPath = Paths.get(targetDirectoryString);
+		if(notExists(targetPath))
+		{
+			try
+			{
+				createDirectory(targetPath);
+				LOGGER.info("Created Folder " + targetPath);
+			} catch(IOException ex)
+			{
+				LOGGER.log(Level.SEVERE, "Exception: target DIR cannot be created " + ex.getMessage());
+				throw ex;
+			}
+		}
+
+		// The entries returned by the iterator are filtered by matching the String representation of their file names against the given pattern.
+		try(DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(sourceDirectoryString), pattern))
+		{
+			for(Path entry : stream)
+			{
+				// fileName = "[HorribleSubs] Hunter x Hunter - 138 [720p]" => shortFileName = "Hunter x Hunter"
+				String fileName = entry.getFileName().toString();
+				String shortFileName = fileName.substring(fileName.indexOf(']') + 2, fileName.lastIndexOf('-') - 1);
+				// Function resolve() makes a new path from a given path and a string
+				Path folderDestinationPath = targetPath.resolve(shortFileName);
+				Path fileDestinationPath = folderDestinationPath.resolve(fileName);
+				try
+				{
+					if(notExists(folderDestinationPath))
+					{
+						createDirectories(folderDestinationPath);
+						LOGGER.info("Created Folder " + folderDestinationPath);
+					}
+					if(!tryMove(entry, fileDestinationPath)) {
+                        Files.copy(entry, fileDestinationPath, REPLACE_EXISTING);
+						LOGGER.info(entry + " COPIED TO " + fileDestinationPath + "\nPlease delete manually! ");
+					}
+
+				} catch(IOException ex)
+				{
+					LOGGER.log(Level.SEVERE, "Exception:  " + ex.getMessage());
+				}
+			}
+		} catch(IOException | DirectoryIteratorException ex)
+		{
+			LOGGER.log(Level.SEVERE, "Exception: " + ex.getMessage());
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AnimeSorter sorterObject = new AnimeSorter();
+			sorterObject.moveFiles();
+
+		} catch(Exception ex)
+		{
+			LOGGER.log(Level.SEVERE, "Exception: " + ex.getMessage());
+		}
+		//LOGGER.info("***AnimeSorter Process Finished***");
+	}
 
 }
